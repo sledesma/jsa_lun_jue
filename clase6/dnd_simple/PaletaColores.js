510d81e85b8ee424cf5ba33414525b4da780b1fa
sledesma <sebas@Scorpion.sebas.com>
2021-05-06 21:39:46 -0300

    Primera version formulario completo

--- clase6/dnd_simple/PaletaColores.js
+++ clase6/dnd_simple/PaletaColores.js
@@ -5,6 +5,7 @@ function PaletaColores(opciones) {
   const main = document.querySelector(opciones.dropzone);
   const circulos = document.querySelectorAll(opciones.items);
 
+  
   // Eventos relativos a la ZONA DE ARRASTRE
 
   main.addEventListener('dragover', function(e){
@@ -19,6 +20,7 @@ function PaletaColores(opciones) {
     });
     PaletaColoresEventTarget.dispatchEvent(new Event('terminoOperacion'));
   });
+
 
   // Eventos relativos a los ITEMS QUE SE ARRASTRAN
 
---
+++ clase6/formulario_2.0/Formulario.js
@@ -0,0 +1,60 @@
+const filezone = document.querySelectorAll('.filezone');
+const btnEnviar = document.querySelector('button');
+
+const archivosSubidos = [];
+
+filezone.forEach(function(fz){
+  fz.addEventListener('dragover', function(e){
+    e.preventDefault();
+    fz.classList.add('over');
+  });
+  
+  fz.addEventListener('dragleave', function(){
+    fz.classList.remove('over');
+  })
+
+  fz.addEventListener('drop', function(e){
+    e.preventDefault();
+    fz.classList.remove('over');
+    const archivos = e.dataTransfer.files
+    for(let i = 0; i < archivos.length; i++) {
+      const actual = archivos[i]
+
+      archivosSubidos.push({
+        nombre: actual.name,
+        url: URL.createObjectURL(archivos[i]),
+        file: archivos[i]
+      });
+
+      fz.innerHTML = archivosSubidos.reduce(function(prev, actual){
+        return prev + '<div>'+actual.nombre+'</div>'
+      }, "");
+
+      console.log("ARCHIVOS SUBIDOS: ", archivosSubidos);
+    }
+  });
+})
+
+btnEnviar.addEventListener('click', function(){
+  const xhr = new XMLHttpRequest();
+  const fd = new FormData();
+
+  fd.append('nombre', document.querySelector('#txtNombre').value)
+  fd.append('apellido', document.querySelector('#txtApellido').value)
+  fd.append('email', document.querySelector('#txtEmail').value)
+  fd.append('telefono', document.querySelector('#txtTelefono').value)
+
+  for(let i = 0; i < archivosSubidos.length; i++) {
+    fd.append('archivo_'+i, archivosSubidos[i].file);
+  }
+
+  xhr.open('POST', 'enviar.php');
+  xhr.send(fd);
+
+  xhr.addEventListener('load', function(){
+    console.log(xhr.response);
+  });
+});
+
+// blob:http://127.0.0.1:5500/67017347-8d24-4689-aea0-9bf8a45b7bc4
+// blob:http://127.0.0.1:5500/355bfd17-b261-4f99-b9ba-00b87e891a5b
