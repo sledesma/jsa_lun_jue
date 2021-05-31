76f4a98527fa7f58442355ad26a337063903a589
sledesma <sebas@Scorpion.sebas.com>
2021-05-31 19:59:00 -0300

    Db Picsum

---
+++ clase12/galeria_fotos/server/src/db.js
@@ -0,0 +1,12 @@
+module.exports = [
+  'https://i.picsum.photos/id/1067/500/500.jpg?hmac=34pP6bV4xWgaFHXjGboN40F9Gy8esUbPYL78s78eAt0',
+  'https://i.picsum.photos/id/984/500/500.jpg?hmac=pyY7pQAMjNdqpFpf7EGBwLWshfW7o1ql2QIDKdwBNAU',
+  'https://i.picsum.photos/id/659/500/500.jpg?hmac=MMLpzsY8n8kf6wezRVrAySdysHV8Iw-5rKzJ01ThP-c',
+  'https://i.picsum.photos/id/665/500/500.jpg?hmac=ToClr1oOiHIhiYrmtsL0YPTSOxpetwIP13CtIYEfdEM',
+  'https://i.picsum.photos/id/985/500/500.jpg?hmac=eWDl4YCKkTZg7VCIhqkkovbeNVY1e86vjOJCo-ClLBQ',
+  'https://i.picsum.photos/id/498/500/500.jpg?hmac=SF-FY1_N6Bv4wjINWBM79Ag8SeJ5si3QgzCDGYJubEs',
+  'https://i.picsum.photos/id/790/500/500.jpg?hmac=9Bblxy_tNyi7S_9t_K0ttju-ZJXFkkVKHMxKe2UGn8k',
+  'https://i.picsum.photos/id/466/500/500.jpg?hmac=sS6QyHpWeyunakCmVkBPXLdAtBCd__va2FGS_Oxs63o',
+  'https://i.picsum.photos/id/0/500/500.jpg?hmac=GxFxxOH5UMJ_PdDE2Yn6uUIaPjoc_UEOUOYzwFiTxMw',
+  'https://i.picsum.photos/id/585/500/500.jpg?hmac=y74bZuN1-WyKKH0HUNFw_sbzKMFmfQ4B6NaCdQvIE8Q'
+]
